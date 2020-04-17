1a2edfcb4342c12f3f6819102f67c889db09ecbe
Varun Krishna Pappusetti <vpkrishna99@gmail.com>
2020-04-17 13:18:14 -0700

    Created RoomProvider class, matches AbstractFactory Pattern

---
+++ TermProject/src/TermProject/RoomProvider.java
@@ -0,0 +1,12 @@
+package TermProject;
+
+public abstract class RoomProvider {
+	/**
+	 * Creates an instance of Room based on the type of Room
+	 * @param roomType of Room
+	 * @return the Room itself
+	 */
+	public static Room getRoom(String roomType) {
+		if (roomType.equals())
+	}
+}
