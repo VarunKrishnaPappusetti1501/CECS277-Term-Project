4430f339c2066ed64fe5b36bfafa9ee3e1a0bb5e
Varun Krishna Pappusetti <vpkrishna99@gmail.com>
2020-04-17 10:25:44 -0700

    SingletonRunner test completed

--- TermProject/src/TermProject/SingletonRunner.java
+++ TermProject/src/TermProject/SingletonRunner.java
@@ -3,7 +3,10 @@ package TermProject;
 public class SingletonRunner {
 
 	public static void main(String[] args) {
-		
+		// Gets a single initialized RoachMotel
+		RoachMotel test = RoachMotel.getRoachMotel();
+		test.setName("RoachMotel");
+		test.getName();
 
 	}
 
